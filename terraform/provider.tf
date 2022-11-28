0aa5630e484a5f2b53fd219d4d47fbc0dc04ef53
Madni MD Shahed <113008512+shahedmadni78@users.noreply.github.com>
2022-11-28 21:06:40 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA3FA5VJUAYBD4I5C6"
-  secret_key = "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"
+  access_key = "AKIARLSKQMFSM2TC4FOR"
+  secret_key = "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"
 }
 
 
